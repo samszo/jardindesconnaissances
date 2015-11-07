b6be1204993924e4e82bf9ded9b3373b7f1d4a16
Samuel Szoniecky <samszon@gmail.com>
2015-11-07 08:00:58 +0100

    corrections

--- application/controllers/BiolographesController.php
+++ application/controllers/BiolographesController.php
@@ -14,7 +14,7 @@ class BiolographesController extends Zend_Controller_Action {
 	 * The default action - show the home page
 	 */
 	public function indexAction() {
-		$this->view->title = "Visualisations disponibles";
+		$this->view->title = "Editeurs de réseaux d'influences";
 	    $this->view->headTitle($this->view->title, 'PREPEND');
 	}
 
